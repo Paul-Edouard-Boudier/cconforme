d0decfd41cc1e9e7a4fbd7811886df70ef213091
Yohan <yarnoux69@gmail.com>
2017-12-06 17:16:48 +0100

    config FOS

---
+++ src/cpossibleBundle/Entity/User.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace cpossibleBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\AttributeOverrides;
+use Doctrine\ORM\Mapping\AttributeOverride;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ *
+ * @AttributeOverrides({
+ *     @AttributeOverride(name="emailCanonical",
+ *         column=@ORM\Column(
+ *             name="emailCanonical",
+ *             type="string",
+ *             length=255,
+ *             unique=false
+ *         )
+ *     )
+ * })
+ */
+
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
+
+?>
