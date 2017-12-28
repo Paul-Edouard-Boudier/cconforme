843bcaa7389f977533ba0602da0a203d29b3530b
Yohan <yarnoux69@gmail.com>
2017-12-28 16:51:47 +0100

    report+modal

---
+++ src/cpossibleBundle/Controller/ReportController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace cpossibleBundle\Controller;
+
+use cpossibleBundle\Entity\Report;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ReportController extends Controller
+{
+    public function indexAction() {
+
+        return $this->render('cpossibleBundle:Report:index.html.twig', array(
+            // ...
+        ));
+    }
+
+    public function formAction() {
+        $test = 'Ceci représente un test de niveau 1';
+        return $this->render('cpossibleBundle:Report:new.html.twig', [
+            'test' => $test,
+        ]);
+    }
+
+    public function insertOneAction(Request $request) {
+        // dump($request->request);die;
+        $em = $this->getDoctrine()->getManager();
+
+        $report = new Report();
+        $report->setAdresseErp($request->request->get('adresse_erp'));
+        $report->setAdresseSignalee($request->request->get('adresse_signalee'));
+        $report->setUserEmail($request->request->get('email'));
+        $report->setMessage($request->request->get('message'));
+        // dump($report);die;
+        $em->persist($report);
+        $em->flush();
+
+        $em = $this->getDoctrine()->getManager();
+        $reports = $em->getRepository('cpossibleBundle:Report')->findAll();
+        return $this->render('cpossibleBundle:Report:index.html.twig', [
+            'reports' => $reports,
+        ]);
+    }
+
+}
---
+++ src/cpossibleBundle/Entity/Report.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace cpossibleBundle\Entity;
+
+/**
+ * Report
+ */
+class Report
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $adresseErp;
+
+    /**
+     * @var string
+     */
+    private $adresseSignalee;
+
+    /**
+     * @var string
+     */
+    private $userEmail;
+
+    /**
+     * @var string
+     */
+    private $message;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set adresseErp
+     *
+     * @param string $adresseErp
+     *
+     * @return Report
+     */
+    public function setAdresseErp($adresseErp)
+    {
+        $this->adresseErp = $adresseErp;
+
+        return $this;
+    }
+
+    /**
+     * Get adresseErp
+     *
+     * @return string
+     */
+    public function getAdresseErp()
+    {
+        return $this->adresseErp;
+    }
+
+    /**
+     * Set adresseSignalee
+     *
+     * @param string $adresseSignalee
+     *
+     * @return Report
+     */
+    public function setAdresseSignalee($adresseSignalee)
+    {
+        $this->adresseSignalee = $adresseSignalee;
+
+        return $this;
+    }
+
+    /**
+     * Get adresseSignalee
+     *
+     * @return string
+     */
+    public function getAdresseSignalee()
+    {
+        return $this->adresseSignalee;
+    }
+
+    /**
+     * Set userEmail
+     *
+     * @param string $userEmail
+     *
+     * @return Report
+     */
+    public function setUserEmail($userEmail)
+    {
+        $this->userEmail = $userEmail;
+
+        return $this;
+    }
+
+    /**
+     * Get userEmail
+     *
+     * @return string
+     */
+    public function getUserEmail()
+    {
+        return $this->userEmail;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     *
+     * @return Report
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+}
