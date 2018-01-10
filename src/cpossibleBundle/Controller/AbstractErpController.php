8c59e088a79efb67707e5eaf62d08fc33b66951c
paul-edouard <paul.edouard.boudier@gmail.com>
2018-01-10 10:30:01 +0100

    ok so i retrieved earlier code to display the date of the message when we search for a particular erp, valid again

--- src/cpossibleBundle/Controller/AbstractErpController.php
+++ src/cpossibleBundle/Controller/AbstractErpController.php
@@ -5,24 +5,18 @@
  * Date: 19/12/2017
  * Time: 15:17
  */
-
 namespace cpossibleBundle\Controller;
-
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 abstract class AbstractErpController extends Controller
 {
-
+  // I don't know what's thats for, no time to go deep in it
     protected function getSingleErp($searchParam) {
         $term = trim(strip_tags($searchParam));
-
         if (is_numeric(mb_substr($term, 0, 1))) {
             $term = explode(" ",$term);
             unset($term[0]);
             $term = implode(" ",$term);
         }
-
         if (is_numeric(mb_substr($term, sizeof($term) - 5, sizeof($term)))) {
             $term = mb_substr($term, 0, sizeof($term) - 6);
         }
@@ -31,16 +25,14 @@ abstract class AbstractErpController extends Controller
             "listeerpNomVoie" => $term
         ]);
     }
-
+    // I don't know what's thats for either
     protected function getErpByType($type, $searchParam) {
         $term = trim(strip_tags($searchParam));
-
         if (is_numeric(mb_substr($term, 0, 1))) {
             $term = explode(" ",$term);
             unset($term[0]);
             $term = implode(" ",$term);
         }
-
         $em = $this->getDoctrine()->getManager();
         return $em->getRepository('cpossibleBundle:DbaListeerp')->createQueryBuilder('a')
             ->where('a.'.$type.' like :value')
@@ -48,64 +40,13 @@ abstract class AbstractErpController extends Controller
             ->getQuery()
             ->getResult();
     }
-
     protected function parseErpEntity($erpEntity) {
-
-        $date = $erpEntity->getListeerpDateValidAdap();
-
-        $delai = $erpEntity->getListeerpDelaiAdap();
-
-        $date_explode = explode("-", $date);
-
-        $date_explode[2] = $date_explode[2] + $delai;
-
-        $date = implode("-", $date_explode);
-
-        switch ($date_explode[1]) {
-            case "01":
-                $date = "Janvier" . " " . $date_explode[2];
-                break;
-            case "02":
-                $date = "Février" . " " . $date_explode[2];
-                break;
-            case "03":
-                $date = "Mars" . " " . $date_explode[2];
-                break;
-            case "04":
-                $date = "Avril" . " " . $date_explode[2];
-                break;
-            case "05":
-                $date = "Mai" . " " . $date_explode[2];
-                break;
-            case "06":
-                $date = "Juin" . " " . $date_explode[2];
-                break;
-            case "07":
-                $date = "Juillet" . " " . $date_explode[2];
-                break;
-            case "08":
-                $date = "Août" . " " . $date_explode[2];
-                break;
-            case "09":
-                $date = "Septembre" . " " . $date_explode[2];
-                break;
-            case "10":
-                $date = "Octobre" . " " . $date_explode[2];
-                break;
-            case "11":
-                $date = "Novembre" . " " . $date_explode[2];
-                break;
-            case "12":
-                $date = "Décembre" . " " . $date_explode[2];
-                break;
-        }
-
         $entity = [
             'name' => $erpEntity->getListeErpNomErp(),
             'adress' => $erpEntity->getListeerpNomVoie(),
             'type' => $erpEntity->getListeerpTypedossier(),
             'demandeur' => $erpEntity->getListeerpDemandeur(),
-            'date' => $date,
+            'date' => $erpEntity->getListeerpDateValidAdap(),
             'delai' => $erpEntity->getListeerpDelaiAdap(),
         ];
         if ($erpEntity->getListeerpNumeroVoie() != '') $entity['adress'] = $erpEntity->
@@ -113,7 +54,6 @@ abstract class AbstractErpController extends Controller
             $erpEntity->getListeerpCodePostal();
         return $entity;
     }
-
     protected function parseErpEntities($erpEntity) {
         $entities = [];
         foreach($erpEntity as $key => $entity) {
@@ -130,5 +70,4 @@ abstract class AbstractErpController extends Controller
         }
         return $entities;
     }
-
 }
--- src/cpossibleBundle/Controller/HomeController.php
+++ src/cpossibleBundle/Controller/HomeController.php
@@ -136,38 +136,36 @@ class HomeController extends AbstractErpController
 
     }
 
-    /**
+      /**
      * @param $status 'ok' | 'ko'
      * @param null $erp ErpEntity
      * @return array
      */
     private function constructResponseMessage($status, $erp = null) {
-      // setlocale(LC_TIME, 'fr_FR.UTF8', 'fr.UTF8', 'fr_FR.UTF-8', 'fr.UTF-8');
-      // $date = $erp["date"];
-      // $delai = $erp["delai"];
-      // //
-      // $date = new DateTime($date);
-      // $mois = ucfirst(strftime("%B", $date->getTimestamp()));
-      // //
-      // $date->add(new DateInterval('P'.$delai.'Y'));
-      // $newdate =  $date->format('d-m-Y');
-      //
-      // $time = strtotime($newdate);
-      // $annee = date("Y",$time);
+        setlocale(LC_TIME, 'fr_FR.UTF8', 'fr.UTF8', 'fr_FR.UTF-8', 'fr.UTF-8');
+        $date = $erp["date"];
+        $delai = $erp["delai"];
+        //
+        $date = new DateTime($date);
+        $mois = ucfirst(strftime("%B", $date->getTimestamp()));
+        //
+        $date->add(new DateInterval('P'.$delai.'Y'));
+        $newdate =  $date->format('d-m-Y');
+        $time = strtotime($newdate);
+        $annee = date("Y",$time);
         $messageText = [
             'standard' => "Sauf erreur et en prenant en compte les précautions d’usage listées ci-dessus, ",
-            'pending' => "L'établissement situé à cette adresse a déclaré être rentrés dans la démarche de mise en accessibilité.",
-            'none' => "Aucun établissement situé à cette adresse n’a déclaré être rentré dans la démarche de mise en accessibilité."
+            'pending' => "l'établissement situé à cette adresse a déclaré être rentrés dans la démarche de mise en accessibilité.",
+            'none' => "aucun établissement situé à cette adresse n’a déclaré être rentré dans la démarche de mise en accessibilité."
         ];
-
         switch($status){
             case 'ok':
                 $response['status'] = 'ok';
-                if ($erp['type'] == 'adap' or 'at-adap') {
-                    $response["message"] = $messageText['pending'] . " Le demandeur " . $erp["demandeur"] .
+                if ($erp['type'] == 'adap') {
+                    $response["message"] = $messageText['standard'] . $messageText['pending'] . " Le demandeur " . $erp["demandeur"] .
                         " s’est engagé à rendre l’ERP " . $erp["name"] . ", situé au " . $erp["adress"] .
                         " conforme à la réglementation en matière d’accessibilité des personnes en situation de handicap avant " .
-                        $erp["date"] . ".";
+                        $mois . " " .$annee. ".";
                 } else {
                     $response["message"] = "Le demandeur " . $erp["demandeur"] . "a déclaré l’établissement " .
                         $erp['name'] . ", situé au " . $erp["adress"] .
@@ -177,34 +175,11 @@ class HomeController extends AbstractErpController
                 return $response;
             case 'ko':
                 return [
-                    "message" => $messageText['none'],
+                    "message" => $messageText['standard'] . $messageText['none'],
                     "status" => 'ko'
                 ];
             default:
                 return;
-        }
-    }
-
-    /**
-     * @param $erpRepo 'ErpRepository'
-     * @param $param array
-     * @return 'ErpEntityArray'
-     */
-    private function getErpEntity($erpRepo, $param) {
-        if ($erpEntity = $erpRepo->findOneBy($param)) {
-            $erp = [
-                'name' => $erpEntity->getListeErpNomErp(),
-                'adress' => $erpEntity->getListeerpNomVoie(),
-                'type' => $erpEntity->getListeerpTypedossier(),
-                'demandeur' => $erpEntity->getListeerpDemandeur(),
-                'date' => $erpEntity->getListeerpDateValidAdap(),
-                'delai' => $erpEntity->getListeerpDelaiAdap(),
-            ];
-            if ($erpEntity->getListeerpNumeroVoie() != '') $erp['adress'] = $erpEntity->
-                getListeerpNumeroVoie() . ' ' . $erpEntity->getListeerpNomVoie();
-            return $erp;
-        }else {
-            return false;
         }
     }
 }
