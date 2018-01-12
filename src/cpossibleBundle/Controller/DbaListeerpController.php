50f727dff07f11e9f06799efa1c6d5264c002455
PaulEdouard Boudier <paul-edouard.boudier@laposte.net>
2018-01-12 12:24:31 +0100

    my entity is fully working + the id is good, need to check for some bug maybe now"
    "

--- src/cpossibleBundle/Controller/DbaListeerpController.php
+++ src/cpossibleBundle/Controller/DbaListeerpController.php
@@ -366,7 +366,6 @@ class DbaListeerpController extends Controller
      * and then update the database if the element doesn't exist already
      */
     public function tpsAction() {
-      echo '<pre>';
       //$procedure = $response->body->procedure;
       // On peut très bien faire une requete de procédure dynamique (donc en checkant le dpt par ex)
       // Et faire une requete de dossier pour chaque dossier trouvé
@@ -379,6 +378,7 @@ class DbaListeerpController extends Controller
       $dossiers =$response->body->dossiers;
       $em = $this->getDoctrine()->getManager();
       foreach ($dossiers as $dossier) {
+        // dump($dossier->id);
         $subResponse = Unirest\Request::get('https://tps.apientreprise.fr/api/v1/procedures/'.$procedure.'/dossiers/'.$dossier->id.'?token='.$token.'');
         $entity = $subResponse->body->dossier;
         $erp = new Dbalisteerp();
@@ -387,123 +387,173 @@ class DbaListeerpController extends Controller
         foreach ($entity->champs as $champ) {
           $libelle = $champ->type_de_champ->libelle;
           $value = $champ->value;
-          // /!\ CURRENTLY WORKING /!\
 
-          // if ($libelle == 'Département') {
-          //   // Regex to get the first 1 to 3 digits that hold the dpt number
-          //   $selector = "/[0-9]{1,3}/";
-          //   preg_match($selector, $value, $departement);
-          //   // dump($departement[0]);
-          //   $erp->setListeerpDepartement($departement[0]);
-          // }
-          // if ($libelle == "Type d'établissement") {
-          //   $nature = strtolower($value);
-          //   // dump($nature);
-          //   $erp->setListeerpNature($nature);
-          // }
-          // if ($libelle == "Catégorie") {
-          //   $selector = "/[0-9]{1,2}/";
-          //   preg_match($selector, $value, $categorie);
-          //   // dump($categorie[0]);
-          //   $erp->setListeerpCategorie($categorie[0]);
-          // }
-          // if ($libelle == "Date de mise en conformité") {
-          //   // Not sure if it's the right date
-          //   // dump($value);
-          //   $erp->setListeerpDateValidAdap($value);
-          // }
-          // if ($libelle == "Durée de la dérogation en année") {
-          //   if (!empty($value)) {
-          //     // dump($value);
-          //     $erp->setListeerpDelaiAdap($value);
-          //   }
-          // }
-          // if ($libelle == "Numéro d'adap") {
-          //   if (!empty($value)) {
-          //     // dump($value);
-          //     $erp->setListeerpIdAdap($value);
-          //   }
-          // }
-          // if ($libelle == "Nom de l'établissement, enseigne") {
-          //   // dump($value);
-          //   $erp->setListeErpNomErp($value);
-          // }
-          // if ($libelle == "Siret") {
-          //   if (empty($value)) {
-          //     // dump($value);
-          //     $erp->setListeerpSiret(0);
-          //   }
-          //   else {
-          //     // dump(intval($value));
-          //     $erp->setListeerpSiret(intval($value));
-          //   }
-          // }
-          // if ($libelle == "Type de déclaration") {
-          //   //dump(strtolower($value));
-          //   $erp->setListeerpTypedossier(strtolower($value));
-          // }
-          // if ($libelle == "Types d'activités") {
-          //   // select all character of $value between A-Z followed by whitespace
-          //   $selector = "/[A-Z]{1,3}\s/";
-          //   preg_match_all($selector, $value, $typesUgly);
-          //   // delete whitespace from string that the previous regex returns us
-          //   $typesBeautified = preg_replace('/\s+/', '', $typesUgly[0]);
-          //   $types = "";
-          //   $last = count($typesBeautified);
-          //   $i = 0;
-          //   foreach ($typesBeautified as $type) {
-          //     if ($i == $last - 1) {
-          //       $types .= $type;
-          //     }
-          //     else {
-          //       $types .= $type.',';
-          //     }
-          //     $i ++;
-          //   }
-          //   // dump($types);
-          //   $erp->setListeerpType($types);
-          // }
-          // if ($libelle == "Adresse") {
-          //   // Regex that split a string like this:
-          //   // 11 Place de l'Europe 69006 Lyon
-          //   // into an array like this:
-          //   // ["full_adress_here","11", "Place de l'Europe", "69006", "Lyon"];
-          //   $selector = "/(\d{1,4})\s?([a-zA-ZÀ-Ÿ',\s]*\s?)?\s?([0-9]{5})\s?([a-zA-ZÀ-Ÿ',\/]*\s*)/";
-          //   preg_match($selector, $value, $splitedAdress);
-          //   $numero_de_voie = $splitedAdress[1];
-          //   $erp->setListeerpNumeroVoie($numero_de_voie);
-          //   // // Adress that we need to write like "PL DE L'EUROPE"
-          //   $tempAdress = $splitedAdress[2]; // Here: "Place de l'Europe "
-          //   $adressExploded = explode(" ",$tempAdress);
-          //   $intitule_voie = $adressExploded[0]; // "Place"
-          //
-          //   // We search in ddb the "intitulevoie" that match with what we get from the "dossier"
-          //   $q = $em->getRepository('cpossibleBundle:DbaIntitulevoie')->createQueryBuilder('v');
-          //   $q->andWhere('v.intitulevoieNom LIKE :intitulevoieNom')
-          //     ->setParameter('intitulevoieNom', '%' . $intitule_voie . '%' );
-          //   $result = $q->getQuery();
-          //   // Here we want to get the name of voie as we wish to put in ddb like "PL"
-          //   $arrayDDB = $result->getArrayResult(); // array of 1 array coming from ddb searching via infos
-          //   $voie = $arrayDDB[0]['intitulevoieCode']; // here we get the "PL"
-          //   $fulladress = "";
-          //   $fulladress .= $voie;
-          //   for ($i=1; $i < count($adressExploded) ; $i++) {
-          //     $fulladress .= " " .strtoupper($adressExploded[$i]);
-          //   }
-          //   // supress the last whitespace form the stirng
-          //   $erp->setListeerpNomVoie(rtrim($fulladress));
-          //
-          //   $codePostal = $splitedAdress[3];
-          //   $erp->setListeerpCodePostal($codePostal);
+          if ($libelle == 'Département') {
+            // Regex to get the first 1 to 3 digits that hold the dpt number
+            // $selector = "/[0-9]{1,3}/";
+            preg_match("/[0-9]{1,3}/", $value, $departement);
+            // dump($departement[0]);
+            $erp->setListeerpDepartement($departement[0]);
+            // Set an id like this: dpt_number + 6 digits(dossier id)
+            // take length of dossier id
+            // $length_dossier_id = strlen(strval($dossier->id));
+            // dump($length_dossier_id);
+            if (strlen(strval($dossier->id)) < 6) {
+              // Check length of dossier->id and add as many 0 to fill the 6 digits that we need for the $erp->id
+              $stringId = $departement[0].str_repeat("0", 6 - strlen(strval($dossier->id))).strval($dossier->id);
+              $erp->setListeerpId(intval($stringId));
+            } else {
+              $stringId = $departement[0].strval($dossier->id);
+              $erp->setListeerpId(intval($stringId));
+            }
+          }
+          if ($libelle == "Type d'établissement") {
+            // $nature = strtolower($value);
+            // dump($nature);
+            $erp->setListeerpNature(strtolower($value));
+          }
+          if ($libelle == "Catégorie") {
+            // $selector = "/[0-9]{1,2}/";
+            preg_match("/[0-9]{1,2}/", $value, $categorie);
+            // dump($categorie[0]);
+            $erp->setListeerpCategorie($categorie[0]);
+          }
+          if ($libelle == "Date de mise en conformité") {
+            // Not sure if it's the right date
+            // dump($value);
+            $erp->setListeerpDateValidAdap($value);
+          }
+          if ($libelle == "Durée de la dérogation en année") {
+            if (!empty($value)) {
+              // dump($value);
+              $erp->setListeerpDelaiAdap($value);
+            }
+          }
+          if ($libelle == "Numéro d'adap") {
+            if (!empty($value)) {
+              // dump($value);
+              $erp->setListeerpIdAdap($value);
+            }
+          }
+          if ($libelle == "Nom de l'établissement, enseigne") {
+            // dump($value);
+            $erp->setListeErpNomErp($value);
+          }
+          if ($libelle == "Siret") {
+            if (empty($value)) {
+              // dump($value);
+              $erp->setListeerpSiret(0);
+            }
+            else {
+              // dump(intval($value));
+              $erp->setListeerpSiret(intval($value));
+            }
+          }
+          if ($libelle == "Type de déclaration") {
+            //dump(strtolower($value));
+            $erp->setListeerpTypedossier(strtolower($value));
+          }
+          if ($libelle == "Types d'activités") {
+            // select all character of $value between A-Z followed by whitespace
+            $selector = "/[A-Z]{1,3}\s/";
+            preg_match_all($selector, $value, $typesUgly);
+            // delete whitespace from string that the previous regex returns us
+            $typesBeautified = preg_replace('/\s+/', '', $typesUgly[0]);
+            $types = "";
+            $last = count($typesBeautified);
+            $i = 0;
+            foreach ($typesBeautified as $type) {
+              if ($i == $last - 1) {
+                $types .= $type;
+              }
+              else {
+                $types .= $type.',';
+              }
+              $i ++;
+            }
+            // dump($types);
+            $erp->setListeerpType($types);
+          }
+          if ($libelle == "Adresse") {
+            // var wrote with underscore are the one coming from google
+            // I think that my double checking is useless, maybe it is better to just work with what google gives me
+            // dump($value);
 
-          // TODO: retrieve adresse and ask googlemap api to have the commune + long and lat
-          // }
-          // if ($libelle == "Nom de l'entreprise") {
-          //   $erp->setListeerpDemandeur($value);
-          // }
-          // // For now status is always 0:
-          // $erp->setListeerpStatut(0);
-          // // /!\ END CURRENTLY WORKING /!\
+            // /!\ GOOGLE REQUEST /!\
+            // google request to retrieve lat and long from $value
+            // api key that is in base.html.twig
+            $key = "AIzaSyBapkuSxVaHJ0CZhOBk3H4NnHARd4H_btk";
+            $response = Unirest\Request::get('https://maps.googleapis.com/maps/api/geocode/json?address='.$value.'&key='.$key.'');
+            // $result holds data that we cant use
+            $result = $response->body->results[0];
+            // $adress_components holds every entries for and address
+            $adress_components = $result->address_components;
+            foreach ($adress_components as $adress) {
+              if (in_array('street_number', $adress->types)) {
+                $street_number = $adress->long_name;
+              }
+              if (in_array('postal_code', $adress->types)) {
+                $postal_code = $adress->long_name;
+              }
+              if (in_array('route', $adress->types)) {
+                $route_name = $adress->long_name;
+              }
+              // If we want to retrieve locality via the adress that google found
+              if (in_array('locality', $adress->types)) {
+                $commune_google = $adress->long_name;
+                // dump($commune_google);die;
+              }
+            }
+            $location = $result->geometry->location; // object that contain lat and lng
+            // /!\ END GOOGLE REQUEST /!\
+
+            // Regex that split a string like this:
+            // 11 Place de l'Europe 69006 Lyon
+            // into an array like this:
+            // ["full_adress_here","11", "Place de l'Europe ", "69006", "Lyon"];
+            $selector = "/(\d{1,4})\s?([a-zA-ZÀ-Ÿ',\s]*\s?)?\s?([0-9]{5})\s?([a-zA-ZÀ-Ÿ',\/]*\s*)/";
+            preg_match($selector, $value, $splitedAdress);
+            $numeroVoie = $splitedAdress[1];
+            $codePostal = $splitedAdress[3];
+            $commune = $splitedAdress[4];
+
+            // // Adress that we need to write like "PL DE L'EUROPE"
+            // $tempAdress = $splitedAdress[2]; // Here: "Place de l'Europe "
+            // I take what google gives me cause it's way better than myugly regex
+            // might aswell take everything from google at this point
+            $tempAdress = $route_name; // Here: "Place de l'Europe" (whithout whitespcae at the end)
+            $adressExploded = explode(" ",$tempAdress);
+            $intitule_voie = $adressExploded[0]; // "Place"
+
+            // We search in ddb the "intitulevoie" that match with what we get from the "dossier"
+            $q = $em->getRepository('cpossibleBundle:DbaIntitulevoie')->createQueryBuilder('v');
+            $q->andWhere('v.intitulevoieNom LIKE :intitulevoieNom')
+              ->setParameter('intitulevoieNom', '%' . $intitule_voie . '%' );
+            $result = $q->getQuery();
+            // Here we want to get the nom de voie as we wish to put in ddb like "PL"
+            $arrayDDB = $result->getArrayResult(); // array of 1 array coming from ddb searching via infos
+            $voie = $arrayDDB[0]['intitulevoieCode']; // here we get the "PL"
+            $fulladress = "";
+            $fulladress .= $voie;
+            for ($i=1; $i < count($adressExploded) ; $i++) {
+              $fulladress .= " " .strtoupper($adressExploded[$i]);
+            }
+
+            // Set the object
+            $erp->setListeerpNomVoie($fulladress);
+            // Check if $numeroVoie is the same as the one that google map gives us
+            ($numeroVoie != $street_number)? $erp->setListeerpNumeroVoie($street_number) : $erp->setListeerpNumeroVoie($numeroVoie);
+            // Check if i get the same postal code as google
+            ($codePostal != $postal_code)? $erp->setListeerpCodePostal($postal_code) : $erp->setListeerpCodePostal($codePostal);
+            ($commune != $commune_google)? $erp->setListeerpNomCommune($commune_google) : $erp->setListeerpNomCommune($commune);
+            $erp->setListeerpLatitude($location->lat);
+            $erp->setListeerpLongitude($location->lng);
+          }
+          if ($libelle == "Nom de l'entreprise") {
+            $erp->setListeerpDemandeur($value);
+          }
+          // For now status is always 0:
+          $erp->setListeerpStatut(0);
 
           // $em->persist($erp);
           // $em->flush();
