2dbd44e7c05ba5a7c0b8340b9a24be1cfbfcb1b2
resic69.net <resic-ponnelle@ssh2.20gp.ha.ovh.net>
2018-01-16 14:11:47 +0000

    changed true to false for prod to display basic errors

--- web/app.php
+++ web/app.php
@@ -5,7 +5,7 @@ require __DIR__.'/../vendor/autoload.php';
 if (PHP_VERSION_ID < 70000) {
     include_once __DIR__.'/../var/bootstrap.php.cache';
 }
-$kernel = new AppKernel('prod', true);
+$kernel = new AppKernel('prod', false);
 if (PHP_VERSION_ID < 70000) {
     $kernel->loadClassCache();
 }
