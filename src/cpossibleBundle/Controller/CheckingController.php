78f4d0f349dbd75805f5e9f070ec5f89710e89d9
PaulEdouard Boudier <paul-edouard.boudier@laposte.net>
2018-02-05 12:52:38 +0100

    added checking for admin

--- src/cpossibleBundle/Controller/CheckingController.php
+++ src/cpossibleBundle/Controller/CheckingController.php
@@ -11,35 +11,44 @@ use Unirest;
 class CheckingController extends Controller {
 
 	public function siretAction(Request $request) {
-		// token given by my tutor;
-		$token = "owliIf6GFqVoA6qASY8NXnoPW3zCliOl";
-		// get siret from request
-		$siret = $request->get('siret');
-		// $tokengoogle = 
-		$response = Unirest\Request::get('https://entreprise.api.gouv.fr/v2/etablissements/'.$siret.'?token='.$token.'');
-		if (!isset($response->body->errors)) {
-			$data['enseigne'] = $response->body->etablissement->enseigne;
-			$data['demandeur'] = $response->body->etablissement->adresse->l1;
-			$fullAdressForSearch = $response->body->etablissement->adresse->l4." ".$response->body->etablissement->adresse->l6;
-			// $key comes from base.html.twig
-			$key = "AIzaSyBapkuSxVaHJ0CZhOBk3H4NnHARd4H_btk";
-			$response = Unirest\Request::get('https://maps.googleapis.com/maps/api/geocode/json?address='.$fullAdressForSearch.'&key='.$key.'');
-			$result = $response->body->results[0];
-			$components = ['street_number', 'route', 'locality', 'administrative_area_level_2', 'postal_code'];
-			$address_components = $result->address_components;
-			foreach ($address_components as $address) {
-				$type = $address->types[0];
-				if (in_array($type, $components)) {
-					$data['address'][$type] = $address->short_name;
+		$securityContext = $this->container->get('security.authorization_checker');
+		if ($securityContext->isGranted('ROLE_SUPER_ADMIN')) {
+			if ($this->getUser() && $this->getUser()->getusername() == 'adminresic') {
+				// token given by my tutor;
+				$token = "owliIf6GFqVoA6qASY8NXnoPW3zCliOl";
+				// get siret from request
+				$siret = $request->get('siret');
+				// $tokengoogle = 
+				$response = Unirest\Request::get('https://entreprise.api.gouv.fr/v2/etablissements/'.$siret.'?token='.$token.'');
+				if (!isset($response->body->errors)) {
+					$data['enseigne'] = $response->body->etablissement->enseigne;
+					$data['demandeur'] = $response->body->etablissement->adresse->l1;
+					$fullAdressForSearch = $response->body->etablissement->adresse->l4." ".$response->body->etablissement->adresse->l6;
+					// $key comes from base.html.twig
+					$key = "AIzaSyBapkuSxVaHJ0CZhOBk3H4NnHARd4H_btk";
+					$response = Unirest\Request::get('https://maps.googleapis.com/maps/api/geocode/json?address='.$fullAdressForSearch.'&key='.$key.'');
+					$result = $response->body->results[0];
+					$components = ['street_number', 'route', 'locality', 'administrative_area_level_2', 'postal_code'];
+					$address_components = $result->address_components;
+					foreach ($address_components as $address) {
+						$type = $address->types[0];
+						if (in_array($type, $components)) {
+							$data['address'][$type] = $address->short_name;
+						}
+					}
+					$data['lat'] = strval($result->geometry->location->lat);
+					$data['lng'] = strval($result->geometry->location->lng);
+					return new JsonResponse($data);
 				}
+				else {
+					$data = 0;
+					return new JsonResponse($data);
+				}
+			} else {
+			  return $this->redirectToRoute('cpossibleBundle:Home:accueil.html.twig');
 			}
-			$data['lat'] = strval($result->geometry->location->lat);
-			$data['lng'] = strval($result->geometry->location->lng);
-			return new JsonResponse($data);
-		}
-		else {
-			$data = 0;
-			return new JsonResponse($data);
+		} else {
+		return $this->redirectToRoute('fos_user_security_login');
 		}
 	}
 
