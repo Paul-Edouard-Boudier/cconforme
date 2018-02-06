5ebffae8405b0dd15fc9b37f7172df9ebe701eab
PaulEdouard Boudier <paul-edouard.boudier@laposte.net>
2018-02-06 14:22:47 +0100

    informations for next dev that will work on implementing users

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,6 +10,39 @@ use Symfony\Component\HttpFoundation\Request;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
+
+// Here is what you need to add for users from where i was working, on branch Users
+
+// vendor/friendofsymfony/user-bundle/ressources/config/doctrine-mapping/user.orm.xml:
+
+        // <field name="departement" column="departement" type="string" length="45" nullable="true"/>
+
+        // <field name="commune" column="commune" type="string" length="255" nullable="true"/>
+
+        // <field name="epci_metropole" column="epci_metropole" type="string" length="255" nullable="true"/>
+
+// vendor/friendofsymfony/user-bundle/Model/User.php: + getter and setter 
+//      /**
+//      *
+//      * @var string
+//      */
+//     protected $departement;
+//  *
+//      *
+//      * @var string
+     
+//     protected $commune;
+
+// /**
+//      *
+//      * @var string
+//      */
+//     protected $epci_metropole;
+// And I think that's it
+
+
+
+
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], ['192.168.33.40', '::1'], true) || PHP_SAPI === 'cli-server')
